9772243c6cf227d04a32069d32a4f83b39043adf
Fernando Gomez <fernandogomezv@gmail.com>
2022-05-18 15:04:36 +0200

    airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating Flats...'
+apt = { name: 'C/ Unanimidad 56', address: 'VV', description: 'italian', price_per_night: 125, number_of_guests: 2 }
+apt2 = { name: 'C/ Unanimidad 57', address: 'VV', description: 'italian', price_per_night: 130, number_of_guests: 3 }
+apt3 = { name: 'C/ Unanimidad 58', address: 'VV', description: 'italian', price_per_night: 135, number_of_guests: 4 }
+apt4 = { name: 'C/ Unanimidad 59', address: 'VV', description: 'italian', price_per_night: 140, number_of_guests: 5 }
+apt5 = { name: 'C/ Unanimidad 60', address: 'VV', description: 'italian', price_per_night: 145, number_of_guests: 6 }
+
+[apt, apt2, apt3, apt4, apt5].each do |attributes|
+  flat = Flat.create!(attributes)
+  puts "Created #{flat.name}"
+end
+puts 'Finished!'
